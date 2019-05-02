8d2b65f5f3c82c98731cc2ad5cb3901e8c9611e3
Irene Fuente <ifuenteromero@gmail.com>
2019-05-02 18:41:15 +0200

    when you click on button you can see pokemon cards

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,8 +5,10 @@ const input4CardsEl = document.getElementById('card4');
 const input6CardsEl = document.getElementById('card6');
 const input8CardsEl = document.getElementById('card8');
 const cardContainer = document.querySelector('.card__container');
+const aux = document.querySelector('.aux');
+let arrayCards =[];
+let arrayImages =[];
 let numberCards =4;
-console.log(cardContainer);
 function getNumberSelected(event){
     numberCards = event.currentTarget.value;
 }
@@ -18,11 +20,27 @@ btnBeginEl.addEventListener('click',getCards);
 
 function getCards(){
     cardContainer.innerHTML='';
-    for (let i=0;i<numberCards;i++){
-        console.log(numberCards);
-       const card = document.createElement('div');
-       cardContainer.appendChild(card);
-       card.classList.add('card');
+    fetch ('https://raw.githubusercontent.com/Adalab/cards-data/master/'+numberCards+'.json')
+    .then(response => response.json())
+    .then(data => {arrayCards = data;
+        for(const obj of arrayCards){
+            
+            const card = document.createElement('div');
+           cardContainer.appendChild(card);
+           const cardImage = document.createElement('img');
+          card.appendChild(cardImage);
+          cardImage.setAttribute('src',obj.image);
+        
+        
+        } });
+        
+        //aux.innerHTML=data[0].name
 
-}
+    // for (let i=0;i<numberCards;i++){
+    //     console.log(numberCards);
+    //    const card = document.createElement('div');
+    //    cardContainer.appendChild(card);
+    //    card.classList.add('card');
+
+//}
 }
