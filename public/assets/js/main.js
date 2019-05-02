8bf6c5556c17f6fc9309e146b2c6b948c2f8bf63
Irene Fuente <ifuenteromero@gmail.com>
2019-05-02 19:38:42 +0200

    change pictures when click

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -6,9 +6,11 @@ const input6CardsEl = document.getElementById('card6');
 const input8CardsEl = document.getElementById('card8');
 const cardContainer = document.querySelector('.card__container');
 const aux = document.querySelector('.aux');
+const defaultImage = 'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB';
 let arrayCards =[];
 let arrayImages =[];
 let numberCards =4;
+
 function getNumberSelected(event){
     numberCards = event.currentTarget.value;
 }
@@ -23,25 +25,38 @@ function getCards(){
     fetch ('https://raw.githubusercontent.com/Adalab/cards-data/master/'+numberCards+'.json')
     .then(response => response.json())
     .then(data => {arrayCards = data;
-        for(const obj of arrayCards){
-            
-            const card = document.createElement('div');
-           cardContainer.appendChild(card);
-           const cardImage = document.createElement('img');
-          card.appendChild(cardImage);
-          cardImage.setAttribute('src',obj.image);
-        
-        
-        } });
-        
-        //aux.innerHTML=data[0].name
+        printDefaultImages(arrayCards) });     
+}
 
-    // for (let i=0;i<numberCards;i++){
-    //     console.log(numberCards);
-    //    const card = document.createElement('div');
-    //    cardContainer.appendChild(card);
-    //    card.classList.add('card');
+function printDefaultImages(arrayCards){
+    let i=0;
+    for(const obj of arrayCards){ 
+        const card = document.createElement('li');
+        cardContainer.appendChild(card);
+        const cardImage = document.createElement('img');
+        card.appendChild(cardImage);
+        card.classList.add('card');
+        cardImage.setAttribute('src',defaultImage);
+        cardImage.setAttribute('id',i);
+        card.addEventListener('click',changeImage );
+        arrayImages.push(obj.image);
+        // console.log(arrayImages);
+        i++;
+    
+    }
+}
 
-//}
+function changeImage(event){
+    const card= event.currentTarget;
+    const cardImage = card.firstChild;
+    const cardImageIndex = cardImage.getAttribute('id');
+    const currentImage = cardImage.src;
+    if (currentImage == defaultImage){
+        cardImage.setAttribute('src',arrayImages[cardImageIndex]);
+    }
+    else{
+        cardImage.setAttribute('src',defaultImage);
+    }
+    
 }
 //# sourceMappingURL=main.js.map
