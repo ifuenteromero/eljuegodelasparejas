dab7c3f0b331ca222da9f88cca6e1e48ef27b10f
Irene Fuente <ifuenteromero@gmail.com>
2019-05-03 21:21:25 +0200

    clean staff from cache

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -8,12 +8,11 @@ const cardContainer = document.querySelector('.card__container');
 const aux = document.querySelector('.aux');
 const defaultImage = 'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB';
 let arrayCards =[];
-
 let arrayObjectsGame = [];
 let arrayObjectsCache=[];
 let numberCards =4;
 getNumberCardsSaved();
-getDataFromCache();
+
 
 function getNumberCardsSaved(){
     const data = localStorage.getItem('numberCards');
@@ -77,13 +76,13 @@ function changeImage(event){
     // console.log(arrayObjectsGame);
     if (currentImage == defaultImage){
         cardImage.setAttribute('src',arrayObjectsGame[cardImageId].image);
-         addObjectCache(obj);
+        
         
        
     }
     else{
         cardImage.setAttribute('src',defaultImage);
-        removeObjectCache(obj);
+       
     }
     
 }
@@ -95,7 +94,6 @@ function createObject(id,image,pair){
         pair:pair,
     }
     return obj;
-    
 
 }
 function addObjectArrayGame(id,image,pair){
@@ -105,48 +103,6 @@ function addObjectArrayGame(id,image,pair){
     //  console.log(arrayObjectsGame);
 }
 
-function addObjectCache(obj){
-    arrayObjectsCache.push(obj);
-    localStorage.setItem('savedPairs',JSON.stringify(arrayObjectsCache));
-}
-function removeObjectCache(obj){
-    arrayObjectsCache = arrayObjectsCache.filter(objeto=>objeto.id!==obj.id);
-    localStorage.setItem('savedPairs',JSON.stringify(arrayObjectsCache));
-}
 
-
-function getDataFromCache(){
-    const data = JSON.parse(localStorage.getItem('savedPairs'));
-    if(data){ arrayObjectsCache =data}else{arrayObjectsCache=[];}
-    const dataGame = JSON.parse(localStorage.getItem('pairsGame'));
-    if (dataGame){arrayObjectsGame=dataGame}else{arrayObjectsGame=[]};
-   
-}
-
-
-console.log(arrayObjectsCache);
-console.log(arrayObjectsGame);
-printFromCache();
-function printFromCache(){
-    for(let i=0;i<arrayObjectsGame.length;i++)
-    { 
-        const card = document.createElement('li');
-        cardContainer.appendChild(card);
-        const cardImage = document.createElement('img');
-        card.appendChild(cardImage);
-        card.classList.add('card');
-        cardImage.classList.add('card__image');
-        const obj = arrayObjectsCache.find(card=>card.id===i);
-        let currentImage=defaultImage;
-        if(obj){currentImage=arrayObjectsGame[i].image;}
-        cardImage.setAttribute('src',currentImage);
-        // addObjectArrayGame(i,arrayCards[i].image,arrayCards[i].pair);
-       
-        cardImage.setAttribute('id',i);
-        card.addEventListener('click',changeImage);
-       
-    }
-    // console.log(arrayObjectsGame);
-}
 
 //# sourceMappingURL=main.js.map
